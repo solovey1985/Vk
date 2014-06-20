b5dafa35b3950055d8dcf059df396fb5d2159e22
solovey1985 <solovey1985@ukr.net>
2014-06-20 17:51:00 +0300

    Long term commit

---
+++ ViewModels/ViewModels/Users/FriendsOnlineVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using ViewModels.ViewModels.Users;
+using Vk.DTO.Domain;
+using Vk.DTO.ViewModels;
+
+namespace Vk.DTO.ViewModels
+{
+    public class FriendsOnlineVM : ViewModel, IFriendsPage
+    {
+        public BaseCommand SendMessage { get; set; }
+        
+        public ObservableCollection<User> Friends { get; set; }
+        public string Name { get; set; }
+        #region .ctor
+
+        public FriendsOnlineVM()
+        {
+                Friends = new ObservableCollection<User>();
+        }
+        #endregion
+    }
+}
---
+++ ViewModels/ViewModels/Users/FriendsVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using ViewModels.ViewModels.Users;
+
+namespace Vk.DTO.ViewModels
+{
+    public class FriendsVM: ViewModel
+    {
+        private FriendsOnlineVM _friendsOnlineVM;
+
+        private ObservableCollection<IFriendsPage> pages;
+        public ObservableCollection<IFriendsPage> Pages{get { return pages; } set { pages = value; }}
+
+
+        public FriendsOnlineVM FriendsOnlineVM
+        {
+            get { return _friendsOnlineVM; }
+            set { _friendsOnlineVM = value; }
+        }
+        public FriendsVM()
+        {
+            pages=new ObservableCollection<IFriendsPage>();
+        }
+
+    }
+}
